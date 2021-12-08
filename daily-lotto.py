3e078e15b7d017ed95cfe57d04d7ef3cf0227b73
Bulelani Botman <botmanbulelani@gmail.com>
2021-12-08 20:05:31 +0200

    added yet again another file to further distinguish the game

---
+++ daily-lotto.py
@@ -0,0 +1,47 @@
+import random
+
+# list to hold the 5 lottory numbers
+numbers = list(range(5))
+
+
+def randomDLBalls():
+    ''' random lottory ball from 1-36 '''
+    return random.randint(1, 36)
+
+
+def lottoryNumbers():
+    ''' a function for assigning random powerballs to the list & validates if the the ball is a one of onen'''
+
+    for number in range(0, 5):
+        numbers[number] = randomDLBalls()
+
+    return numbers
+
+
+def powerballValidator():
+    ''' this method validates if whether the powerball is a one of one '''
+
+    ok = lottoryNumbers()
+    index = 0
+    incrementStopper = 1
+
+    for x in ok:
+
+        for y in ok[incrementStopper:]:
+
+            if x == y:
+
+                ok[index] = randomDLBalls()
+                incrementStopper = incrementStopper + 1
+
+            else:
+
+                incrementStopper = incrementStopper + 1
+
+        index = index + 1
+        incrementStopper = incrementStopper + 1
+
+    return ok
+
+
+print("The Lucky Numbers Are: ", powerballValidator())
